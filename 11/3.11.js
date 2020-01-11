8d94ab4bbaea234b50d68f2a43e499c0d73608fe
Akhat-cpp <56387863+Akhat-cpp@users.noreply.github.com>
2020-01-11 15:24:58 +0200

    Add files via upload

--- 11/3.11.js
+++ 11/3.11.js
@@ -1,18 +1,18 @@
 // 11
 function inpd(str)
 {
-	let arraySHshp = str.split(',');
+	let arraySHshp = str.split(','); //из_строки_убираем_запятые_и_получаем_массив_без_запятых_
 	// alert(arraySHshp)
-	let strSHshp = sArrayVString(arraySHshp);
+	let strSHshp = sArrayVString(arraySHshp);//из_массива_делаем_строку_
 	// alert(strSHshp)
-	let arrayCleanNa05 =  strSHshp.split('https://');
+	let arrayCleanNa05 =  strSHshp.split('https://');//из_строки_убираем_https_и_получаем_массив_без_https_
 	// alert(arrayCleanNa05)
-	let strCleanNa05 = sArrayVString(arrayCleanNa05);
+	let strCleanNa05 = sArrayVString(arrayCleanNa05);//из_массива_делаем_строку_
 	// alert(strCleanNa05)
-	let arrayClean = strCleanNa05.split('http://');
+	let arrayClean = strCleanNa05.split('http://');//из_строки_убираем_http_и_получаем_массив_без_http_
 	// alert(arrayClean)
-	arrayClean.sort();
-	alert(arrayClean[1]);
+	arrayClean.sort();//сортируем_массив_
+	alert(arrayClean[1]);//и_здесь_должна_вывестись_2я_ссылка_(((но_нет(((_
 }
 function sArrayVString(s)
 {
